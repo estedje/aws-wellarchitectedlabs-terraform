3a5180a469df7fc700edc50c3a26aeb4400523c3
estedje <estedje@users.noreply.github.com>
2022-10-01 22:06:35 +0200

    Update module.tf

--- sec_100_create_s3_bucket/module.tf
+++ sec_100_create_s3_bucket/module.tf
@@ -9,3 +9,9 @@ resource "aws_s3_bucket_acl" "wellarchitectedlabs_bucket_1_acl" {
   bucket = aws_s3_bucket.wellarchitectedlabs-bucket-1.id
   acl    = "private"
 }
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.wellarchitectedlabs_bucket_1.id
+  key    = "index"
+  source = "./resources/index.html"
+}
