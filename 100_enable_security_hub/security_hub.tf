bed798657daf48a91cd615011eb9e2af87b67b29
estedje <estedje@users.noreply.github.com>
2022-10-03 10:26:29 +0200

    Create security_hub.tf

---
+++ 100_enable_security_hub/security_hub.tf
@@ -0,0 +1,6 @@
+resource "aws_securityhub_account" "example" {}
+
+resource "aws_securityhub_standards_subscription" "cis_aws_foundations_benchmark" {
+  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+  depends_on    = [aws_securityhub_account.example]
+}
