0c1cf4fc4f07a0a065c6f446c05c8ac8534b464d
estedje <estedje@users.noreply.github.com>
2022-10-04 10:30:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@
 #module "security_hub" {
 #  source = "./100_enable_security_hub"
 #}
-module "security_hub" {
+module "dependency_monitoring" {
   source = "./operational-excellence/100_labs/100_dependency_monitoring"
 }
