54a0324bd5a389d3aabb7ed0989aadafa12ef5f1
estedje <estedje@users.noreply.github.com>
2022-10-04 10:09:47 +0200

    Update module.tf

--- operational-excellence/100_labs/100_dependency_monitoring/module.tf
+++ operational-excellence/100_labs/100_dependency_monitoring/module.tf
@@ -81,7 +81,7 @@ data "aws_ami" "amzn2" {
 
   filter {
     name   = "name"
-    values = ["/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2*"]
+    values = ["amzn2-ami-hvm-*-gp2"]
   }
 
   filter {
