02fc19952daf8afa44f75dea1d524251d5fb25b4
estedje <estedje@users.noreply.github.com>
2022-10-04 09:42:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 #module "s3_buckets" {
 #  source = "./sec_100_create_s3_bucket"
 #}
+#module "security_hub" {
+#  source = "./100_enable_security_hub"
+#}
 module "security_hub" {
-  source = "./100_enable_security_hub"
+  source = "./operational-excellence/100_labs/100_dependency_monitoring"
 }
-
