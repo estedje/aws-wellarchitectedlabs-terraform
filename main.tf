4326cf6afd4b00b61f5c672ae3ae64b7cf57b9e3
Erik Stedje <estedje@users.noreply.github.com>
2022-10-13 10:16:40 +0200

    new module

--- main.tf
+++ main.tf
@@ -8,7 +8,10 @@
 #  source = "./operational-excellence/100_labs/100_dependency_monitoring"
 #  notification_email = var.notification_email
 #}
-module "automating_operations" {
-  source = "./operational-excellence/200_labs/200_automating_operations_with_playbooks_and_runbooks/"  
-  cloud9_owner = var.cloud9_owner
-}  
+#module "automating_operations" {
+#  source = "./operational-excellence/200_labs/200_automating_operations_with_playbooks_and_runbooks/"
+#  cloud9_owner = var.cloud9_owner
+#}
+module "automated_deployment_of_detective_controls" {
+  source = "./operational-excellence/200_labs/200_automating_operations_with_playbooks_and_runbooks/"
+}
